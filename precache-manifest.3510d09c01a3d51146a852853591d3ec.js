608f40230d585567b0de9f119fb8a9821e4ccbf1
Oliver Huynh <haioliver993@gmail.com>
2020-06-01 13:50:58 -0700

    Updates

---
+++ precache-manifest.3510d09c01a3d51146a852853591d3ec.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42dcf897e6c3e942e19b40d29e603a13",
+    "url": "/RobotApp-React/index.html"
+  },
+  {
+    "revision": "b7c72a0d893bd21ddd8c",
+    "url": "/RobotApp-React/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "e5170cf7247f131b71ed",
+    "url": "/RobotApp-React/static/css/main.9b60cc63.chunk.css"
+  },
+  {
+    "revision": "b7c72a0d893bd21ddd8c",
+    "url": "/RobotApp-React/static/js/2.9a215c79.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RobotApp-React/static/js/2.9a215c79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5170cf7247f131b71ed",
+    "url": "/RobotApp-React/static/js/main.c0e7ab75.chunk.js"
+  },
+  {
+    "revision": "a57a805604604a462611",
+    "url": "/RobotApp-React/static/js/runtime-main.d638cfd5.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/RobotApp-React/static/media/SEGA.12a3db8b.woff"
+  }
+]);
